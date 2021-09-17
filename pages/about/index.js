1908083e19fecde0db45ff0bd39643c3d6cfc939
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2021-09-17 20:00:04 +0200

    WIP- added buttons to switch pages and loading animation

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,26 +1,23 @@
 import styles from '../../styles/About.module.scss';
 
 About.title = 'About';
+About.description =
+  "Image data gathered by NASA's Curiosity, Opportunity, and Spirit rovers on Mars";
 function About() {
   return (
     <section className={styles.about}>
       <p>
-        This API is designed to collect image data gathered by NASA&apos;s
-        Curiosity, Opportunity, and Spirit rovers on Mars and make it more
-        easily available to other developers, educators, and citizen scientists.
-        This API is maintained by Chris Cerami. Each rover has its own set of
-        photos stored in the database, which can be queried separately. There
-        are several possible queries that can be made against the API. Photos
-        are organized by the sol (Martian rotation or day) on which they were
-        taken, counting up from the rover&apos;s landing date. A photo taken on
-        Curiosity&apos;s 1000th Martian sol exploring Mars, for example, will
-        have a sol attribute of 1000. If instead you prefer to search by the
-        Earth date on which a photo was taken, you can do that too. Along with
-        querying by date, results can also be filtered by the camera with which
-        it was taken and responses will be limited to 25 photos per call.
-        Queries that should return more than 25 photos will be split onto
-        several pages, which can be accessed by adding a &apos;page&apos; param
-        to the query. Each camera has a unique function and perspective
+        Image data gathered by NASA&apos;s Curiosity, Opportunity, and Spirit
+        rovers on Mars. The API used is maintained by Chris Cerami. Each rover
+        has its own set of photos stored in the database, which can be queried
+        separately. There are several possible queries that can be made against
+        the API. Photos are organized by the sol (Martian rotation or day) on
+        which they were taken, counting up from the rover&apos;s landing date. A
+        photo taken on Curiosity&apos;s 1000th Martian sol exploring Mars, for
+        example, will have a sol attribute of 1000. Along with querying by date,
+        results can also be filtered by the camera with which it was taken and
+        responses will be limited to 25 photos per call. Each camera has a
+        unique function and perspective
       </p>
     </section>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,26 @@
 import styles from '../styles/Home.module.scss';
 
 Home.title = 'Homepage';
+Home.description =
+  'Mars Rover images, includes camera name, date when it was taken and the rover name.';
 export default function Home() {
   return (
-    <>
-      <section className={styles.main}>
-        <h1 className={styles.heading}>Mars Rover Photos</h1>
-      </section>
-    </>
+    <section className={styles.main}>
+      <h1>
+        Image data gathered by NASA&apos;s Curiosity, Opportunity, and Spirit
+        rovers on Mars.
+      </h1>
+
+      <p>
+        The info is fetched via NASA&apos;s public{' '}
+        <a
+          href='https://api.nasa.gov/'
+          target='_blank'
+          rel='noopener noreferrer nofollow'
+        >
+          API
+        </a>
+      </p>
+    </section>
   );
 }
